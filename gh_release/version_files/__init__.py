d7c409f93e58d1aa75ae0919e072f11010c221fa
Mathew Robinson <mathew@chasinglogic.io>
2021-04-10 21:04:39 +0100

    task: add function for updating all version files

--- gh_release/version_files/__init__.py
+++ gh_release/version_files/__init__.py
@@ -1,7 +1,8 @@
-from version_files.cargo_toml import CargoTOML
-from version_files.dunder_version import DunderVersion
-from version_files.package_json import PackageJSON
-from version_files.setup_py import SetupPy
+from gh_release.git import git
+from gh_release.version_files.cargo_toml import CargoTOML
+from gh_release.version_files.dunder_version import DunderVersion
+from gh_release.version_files.package_json import PackageJSON
+from gh_release.version_files.setup_py import SetupPy
 
 VERSION_FILES = [
     PackageJSON,
@@ -9,3 +10,23 @@ VERSION_FILES = [
     DunderVersion,
     CargoTOML,
 ]
+
+
+def update_version_files(version):
+    updated = update_vfs(version)
+    if updated:
+        git("commit", "-m", f"release: {version}")
+        git("push")
+
+
+def update_vfs(version):
+    version_file_updated = False
+    for version_file in []:
+        vf = version_file.from_file()
+        if not vf:
+            continue
+
+        version_file_updated = vf.update(str(version))
+        if version_file_updated:
+            print("Updated version in:", vf.filename)
+    return version_file_updated
