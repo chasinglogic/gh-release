280cb32aab724952bbd3140a4ddf9e5c9cce2c00
Mathew Robinson <mathew@chasinglogic.io>
2021-04-10 21:09:38 +0100

    feature: Add PyProjectTOML version file support

--- gh_release/version_files/__init__.py
+++ gh_release/version_files/__init__.py
@@ -2,11 +2,13 @@ from gh_release.git import git
 from gh_release.version_files.cargo_toml import CargoTOML
 from gh_release.version_files.dunder_version import DunderVersion
 from gh_release.version_files.package_json import PackageJSON
+from gh_release.version_files.pyproject import PyProjectTOML
 from gh_release.version_files.setup_py import SetupPy
 
 VERSION_FILES = [
     PackageJSON,
     SetupPy,
+    PyProjectTOML,
     DunderVersion,
     CargoTOML,
 ]
---
+++ gh_release/version_files/pyproject.py
@@ -0,0 +1,9 @@
+import re
+
+from gh_release.version_files.version_file import VersionFile
+
+
+class PyProjectTOML(VersionFile):
+    filename = "pyproject.toml"
+    pattern = re.compile(r'(version = )"[0-9]*.[0-9]*.[0-9]*"')
+    replace_pattern = r'\1"{new_version}"'
