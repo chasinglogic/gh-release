14473e4b8bfd9d5dd9b3b1f7dba4492f368dbd15
Mathew Robinson <mathew@chasinglogic.io>
2021-04-10 21:03:18 +0100

    fix: add sorted version test

--- tests/test_version.py
+++ tests/test_version.py
@@ -16,3 +16,29 @@ from gh_release.version import Version
 )
 def test_is_version_string(verstring, expected):
     assert Version.is_version_string(verstring) == expected
+
+
+def test_sorted():
+    expected = [
+        Version(0, 1, 0),
+        Version(0, 2, 0),
+        Version(0, 2, 1),
+        Version(0, 3, 0),
+        Version(0, 3, 1, suffix="beta"),
+        Version(0, 3, 1),
+        Version(1, 0, 0),
+    ]
+
+    inputs = [
+        "0.1.0",
+        "0.3.1",
+        "0.2.1",
+        "0.3.1-beta",
+        "0.2.0",
+        "0.3.0",
+        "1.0.0",
+    ]
+
+    versions = [Version.from_str(verstr) for verstr in inputs]
+
+    assert list(sorted(versions)) == expected
