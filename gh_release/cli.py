f67f1525d795b2cbfacdffbfd9b2c3840ffd0260
Mathew Robinson <mathew@chasinglogic.io>
2021-04-10 21:04:04 +0100

    fix: Start splitting up the tool

--- gh_release/cli.py
+++ gh_release/cli.py
@@ -1,66 +1,15 @@
 #!/usr/bin/env python3
 
 import argparse
-from subprocess import check_output
+import os
+import sys
+from copy import deepcopy
 
-import requests
-
-
-def git(*args):
-    cmd = ["git"]
-    cmd.extend(args)
-    return check_output(cmd).decode("utf-8")
-
-
-def get_repo():
-    remotes = check_output(["git", "remote", "-v"]).decode("utf-8").split("\n")
-    for remote in remotes:
-        if not remote:
-            continue
-
-        first_segment, _ = remote.split(" ")
-        _, url = first_segment.split("\t")
-
-        if "github.com" not in url:
-            continue
-
-        if url.startswith("ssh") or url.startswith("git@"):
-            return url.split(":")[-1]
-        else:
-            return "/".join(url.split("/")[-2:])
-
-
-def get_last_version():
-    versions = [
-        Version.from_str(ver[len("refs/tags/") :])
-        for ver in (
-            check_output(
-                [
-                    "git",
-                    "for-each-ref",
-                    "--sort=-taggerdate",
-                    "--format",
-                    "%(refname)",
-                    "refs/tags",
-                ]
-            )
-            .decode("utf-8")
-            .split("\n")
-        )
-        if ver[len("refs/tags/") :]
-    ]
-    if not versions:
-        return Version(0, 0, 0)
-
-    return sorted(versions)[-1]
-
-
-def get_commit_for_tag(tag):
-    return (
-        check_output(["git", "show", "--no-patch", "--pretty=%H", tag])
-        .decode("utf-8")
-        .strip()
-    )
+from gh_release.git import get_commit_for_tag, get_repo, git
+from gh_release.github import create_release
+from gh_release.release import release_notes_for_version
+from gh_release.version import Version
+from gh_release.version_files import update_version_files
 
 
 def main():
@@ -88,7 +37,7 @@ def main():
         "--repo",
         "-r",
         type=str,
-        default=get_repo(),
+        default=None,
         help="The repository to create the release on in :owner/:repo format, "
         "will attempt to parse from git remotes if not given",
     )
@@ -128,16 +77,7 @@ def main():
         print("$GITHUB_TOKEN must be set.")
         sys.exit(1)
 
-    last_version = get_last_version()
-    if last_version == Version(0, 0, 0):
-        commits = git("log", "--reverse", "--format=%H").split("\n")
-        # Initial commit to the branch / repository
-        last_version_commit = commits[0]
-    else:
-        last_version_commit = get_commit_for_tag(str(last_version))
-
-    version_commit = get_commit_for_tag("HEAD")
-
+    last_version_commit, last_version = Version.latest_version()
     if args.version:
         version = Version.from_str(args.version)
     else:
@@ -149,55 +89,14 @@ def main():
         if args.major:
             version.increment_major()
 
-    messages = check_output(
-        [
-            "git",
-            "log",
-            "--pretty=format:%h %s",
-            f"{last_version_commit}...{version_commit}",
-        ],
-    ).decode("utf-8")
-    changelog = "\n".join(
-        [
-            f"- {message}"
-            for message in messages.split("\n")
-            if message
-            and "release: " not in message
-            and "Merge pull request" not in message
-        ],
+    version_commit = get_commit_for_tag("HEAD")
+    release_notes = release_notes_for_version(
+        version,
+        version_commit,
+        last_version_commit,
     )
 
-    emails = check_output(
-        [
-            "git",
-            "log",
-            "--pretty=format:%an <%ae>",
-            f"{last_version_commit}...{version_commit}",
-        ],
-    ).decode("utf-8")
-    contributors = "\n".join({f"- {email}" for email in emails.split("\n") if email})
-
-    if args.title:
-        release_notes = f"# Release {version}\n\n"
-    else:
-        release_notes = ""
-
-    release_notes += (
-        f"{changelog}\n\n## Contributors to this Release\n\n{contributors}\n"
-    )
-
-    body = {
-        "tag_name": str(version),
-        "name": str(version),
-        "body": release_notes,
-        "draft": args.draft,
-        "prerelease": args.pre_release,
-    }
-
-    headers = {
-        "Authorization": f"token {token}",
-        "Accept": "application/vnd.github.v3+json",
-    }
+    repo = args.repo or get_repo()
 
     print("Creating release", version, version_commit)
     print("Previous version", last_version, last_version_commit)
@@ -211,34 +110,25 @@ def main():
     if not ans.startswith("y"):
         return
 
-    version_file_updated = False
-    for version_file in []:
-        vf = version_file.from_file()
-        if not vf:
-            continue
-
-        version_file_updated = vf.update(str(version))
-        if version_file_updated:
-            print("Updated version in:", vf.filename)
-
-    if version_file_updated:
-        git("add", "package.json")
-        git("commit", "-m", f"release: {version}")
-        git("push")
+    if not args.skip_version_files:
+        update_version_files(version)
 
     git("tag", str(version))
     git("push", "--tags")
-    response = requests.post(
-        f"https://api.github.com/repos/{args.repo}/releases",
-        headers=headers,
-        json=body,
-    )
+
     try:
-        response.raise_for_status()
+        create_release(
+            token=token,
+            repo=repo,
+            tag_name=str(version),
+            name=str(version),
+            body=release_notes,
+            draft=args.draft,
+            prerelease=args.prerelease,
+        )
     except Exception as exc:
         print("Failed to create release!")
         print(exc)
-        print(f"{response.text}")
 
 
 if __name__ == "__main__":
