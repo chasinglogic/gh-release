7ed49a86dadb2d567abc91d0db92b10f3a4dcdfc
Mathew Robinson <mathew@chasinglogic.io>
2021-12-16 07:21:59 +0000

    refactor: VersionFile class to be testable

--- release_mgr/version_files/version_file.py
+++ release_mgr/version_files/version_file.py
@@ -13,30 +13,42 @@ class VersionFile:
     pattern = re.compile("")
     replace_pattern = "{new_version}"
 
+    def __init__(self, content=""):
+        self.content = content
+
     @classmethod
     def from_file(cls):
         if os.path.exists(cls.filename):
-            return cls()
+            obj = cls()
+            obj.load_content()
+            return obj
 
         return None
+
+    def load_content(self):
+        with open(self.filename, encoding="utf-8") as existing:
+            self.content = existing.read()
+
+    def subst(self, version):
+        return self.pattern.sub(
+            self.replace_pattern.format(new_version=version),
+            self.content,
+        )
 
     def update(self, version: str):
         if not self.pattern:
             raise Exception("Tried to update a file without a pattern!")
 
-        with open(self.filename, encoding="utf-8") as existing:
-            content = existing.read()
+        if not self.content:
+            self.load_content()
 
-        if self.pattern.search(content) is None:
+        if self.pattern.search(self.content) is None:
             return False
 
-        content = self.pattern.sub(
-            self.replace_pattern.format(new_version=version),
-            content,
-        )
+        new_content = self.subst(version)
 
         with open(self.filename, "w", encoding="utf-8") as new:
-            new.write(content)
+            new.write(new_content)
 
         git("add", self.filename)
         return True
